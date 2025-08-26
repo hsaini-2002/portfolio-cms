8faf6bb7d9b66551e0097671596a6778fd59d51e
hsaini-2002 <harshsainikp2002@gmail.com>
2025-08-26 22:02:59 +0530

    meaningful message about the change

--- config/database.ts
+++ config/database.ts
@@ -73,7 +73,9 @@ export default ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', true),
+      ssl: env.bool('DATABASE_SSL', true)? {
+        rejectUnauthorized: false,
+      } : false,
     },
   },
 });
