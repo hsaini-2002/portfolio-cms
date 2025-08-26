7d73c25f9d00f6984617e9adf50078a943d0cc3d
hsaini-2002 <harshsainikp2002@gmail.com>
2025-08-26 20:59:24 +0530

    meaningful message about the change

--- config/server.ts
+++ config/server.ts
@@ -11,8 +11,9 @@
 
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  port: env.int("PORT", 10000),
   app: {
     keys: env.array("APP_KEYS"),
   },
+  url: env('PUBLIC_URL', 'https://portfolio-cms-a0hn.onrender.com'),
 });
