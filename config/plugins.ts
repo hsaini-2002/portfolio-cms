65e3b721096fa9669e168a897c8d344fbf673a5f
hsaini-2002 <harshsainikp2002@gmail.com>
2025-08-26 15:10:04 +0530

    Update plugin.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: '@strapi/provider-upload-cloudinary',
+        providerOptions: {
+          cloud_name: env('dqdwmctpi'),
+          api_key: env('617338963695839'),
+          api_secret: env('nlcWKAgQ5BEg3J3ywI4VbvP3GM4'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+  });
